c19abd21c015a400caa3ccfec38885911da94e60
MagnavoxTG <dominik.lohner92@gmx.at>
2019-11-18 11:02:39 +0100

    Release 1.0

---
+++ cmake-build-release/CMakeFiles/arithmetic_mean.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/arithmetic_mean.dir/main.c.o"
+  "arithmetic_mean"
+  "arithmetic_mean.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/arithmetic_mean.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
